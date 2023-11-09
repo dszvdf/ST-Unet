50b1de18d182e69a005025025ea231b6d5238cd3
dszvdf <117423196+dszvdf@users.noreply.github.com>
2023-11-09 17:04:48 +0800

    Delete utils directory

--- utils/PSNR.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-# 函数形式，方便计算重建图像的峰值信噪比
-def PSNR_function(prediction, label): # 输出的是tensor类型的一个标量
-    diff = torch.clamp(prediction, 0, 1) - torch.clamp(label, 0, 1)
-    rmse = (diff ** 2).mean().sqrt()
-    psnr = 20 * torch.log10(1 / rmse)
-    return psnr
-    
